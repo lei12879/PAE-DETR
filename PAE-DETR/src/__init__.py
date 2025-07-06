69d5bb5086ae746e2c2fd1c6461f07957d0794ad
lei12879 <leihuan98@163.com>
2025-07-06 20:40:35 +0800

    delete

--- PAE-DETR/src/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Copyright (c) 2024 The D-FINE Authors. All Rights Reserved.
-"""
-
-# for register purpose
-from . import optim
-from . import data
-from . import nn
-from . import zoo
--- PAE-DETR/src/nn/arch/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Copied from RT-DETR (https://github.com/lyuwenyu/RT-DETR)
-Copyright(c) 2023 lyuwenyu. All Rights Reserved.
-"""
-
-
-from .classification import Classification, ClassHead
-from .yolo import YOLO
